1322234e70079e9e0d75b1952e3e6116682b4693
kuma35 <sindobook@gmail.com>
2021-08-16 20:56:22 +0900

    ALLOWD_HOSTS and INSTALLED_APPS
    JP:現在のバージョンでは上記を設定しないとアカンらしい。
    ALLOWD_HOSTSは端末PCがDHCPクライアントのためip範囲を指定したかったが、
    方法が不明だったので仕方なく'*'としてある。

--- rocket/rocket/settings.py
+++ rocket/rocket/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$x7a*g*#k1pv4-07#wp)n=rl)o5nidevg+ebxc=y%5y!pv7i!%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rocket',
 ]
 
 MIDDLEWARE = [
