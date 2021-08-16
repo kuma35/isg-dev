98f9289f6d62636e9906d99e191eee77c5013f86
kuma35 <sindobook@gmail.com>
2021-08-16 20:53:25 +0900

    change import, change paths.
    JP:from ... importの変更を反映。
    urlpatternsの記述が [ ... ] になってるので対応。
    manage.pyが1つ上のフォルダに移動したので、pathをそこからの相対に書き換え。

--- rocket/rocket/urls.py
+++ rocket/rocket/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
+from django.urls import path, path, include
+from rocket.launcher import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('launcher/', include('rocket.launcher.urls')),
+    path('top/', views.index),
+    path('', views.index),  # fault route
 ]
